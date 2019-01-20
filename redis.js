09c17843e884ed417c6895214895a1fded823d1c
chujacky <jacky9407@gmail.com>
2019-01-19 16:27:35 -0800

    Implement k6 and redis for stress test and optimization

---
+++ redis.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Redis client connected');
+});
+
+client.on('error', (err) => {
+  console.log('Something went wrong ' + err);
+});
+
+module.exports = client;
--- script.js
+++ script.js
@@ -3,13 +3,15 @@ import { sleep } from "k6";
 
 export let options = {
   vus: 200,
-  duration: "240s",
+  duration: "300s",
   rps: 2000
 };
 
 export default function() {
   const queryStrings = ['car', 'bacon', 'cotton', 'awesome', 'bike', 'intelligent', 'concrete', 'fresh', 'tasty', 
-  'metal', 'wooden', 'unbranded', 'pratical', 'handmade', 'Tasty', 'Steel', 'Ergonomic', 'Gorgeous'];
+  'metal', 'wooden', 'unbranded', 'practical', 'handmade', 'Tasty', 'Steel', 'Ergonomic', 'Gorgeous', 'computer', 
+  'gloves', 'Granite', 'Table', 'Salad', 'Towels', 'Shoes', 'small', 'refined'];
+
   const query = queryStrings[Math.floor(Math.random() * queryStrings.length)];
   http.get(`http://localhost:3003/products/all/${query}`);
 };
