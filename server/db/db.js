6baaebffa2d858159b3e7e57a9fb19c415328a5a
chujacky <jacky9407@gmail.com>
2019-01-19 16:24:26 -0800

    Clean up code in db

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,11 @@
-// const cassandra = require('cassandra-driver');
-
-// const client = new cassandra.Client({ contactPoints: ['127.0.0.1:9042'], keyspace: 'amazon', localDataCenter: 'datacenter1' } );
-
 const { Pool } = require('pg');
+
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/amazon';
+
 const pool = new Pool({
   host: 'localhost',
   database: 'amazon',
-  max: 300,
+  // max: 300,
 });
 
 pool.connect((err, client, release) => {
@@ -15,44 +13,6 @@ pool.connect((err, client, release) => {
     return console.error('Error acquiring client', err.stack)
   }
 });
-
-// client.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('connected to cassandra');
-// });
-
-// const read = (query, cb) => {
-//   // const categoryParam = req.params.category.toLowerCase();
-//   const queryString = `SELECT * FROM ${query.toLowerCase()[0]} WHERE keyword= '${query.toLowerCase()}' LIMIT 10`;
-//   client.execute(queryString, (err, results) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     cb(null, results.rows);
-//   });
-// };
-
-// client.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('connected to psql');
-// });
-
-// const read = (query, cb) => {
-//   // const categoryParam = req.params.category.toLowerCase();
-//   // const strings = query.split(' ')
-//   const queryString = `SELECT * FROM ${query.toLowerCase()[0]} WHERE keyword = '${query.toLowerCase()}' and popularity > 99  ORDER BY RANDOM() LIMIT 10`;
-//   pool.query(queryString, (err, results) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     // pool.end();
-//     cb(null, results.rows);
-//   });
-// };
 
 module.exports = {
   pool,
